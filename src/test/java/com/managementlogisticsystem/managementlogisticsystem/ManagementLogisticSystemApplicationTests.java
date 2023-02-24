50a0fdffe19ae2f2ca3453fbda6539e19ea6db2c
heinrichlaszlo <43448196+heinrichlaszlo@users.noreply.github.com>
2023-02-24 18:18:23 +0100

    test: test

---
+++ src/test/java/com/managementlogisticsystem/managementlogisticsystem/ManagementLogisticSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managementlogisticsystem.managementlogisticsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementLogisticSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
